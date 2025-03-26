02ae2ecdfeb33bb7de0b4686ffb90c23a3aa235f
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-03-26 12:57:41 +0700

    counttime

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,48 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
-  
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [running]);
 
   return (
     <>
-      hello world
-
+      <h1>01-Stopwatch</h1>
+      <div>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        {running ? (
+          <button onClick={() => setRunning(false)}>Stop</button>
+        ) : (
+          <button onClick={() => setRunning(true)}>Start</button>
+        )}
+        <button
+          onClick={() => {
+            setTime(0);
+            setRunning(false);
+          }}
+        >
+          Reset
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
