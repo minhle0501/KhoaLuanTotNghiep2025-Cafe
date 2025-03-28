2c92d543d25a7e3ded81188a2e0889166ad7f9fb
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-03-28 13:28:00 +0700

    them list gia neu khoong co list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
+import Product from "./page/Product";
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { assets } from "./assets/assets";
 import SearchBar from "./components/SearchBar";
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 //nơi chứa các components chính và định tuyến router
 const App = () => {
   return (
     <div>
-      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"
+      <div
+        className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"
         style={{
           background: `linear-gradient(rgba(0, 0, 0, 0.450), rgba(0, 0, 0, 0.500)), url(${assets.a_cup})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center center',
-        }}>
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+        }}
+      >
         <NavBar />
         <SearchBar />
         <>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/product/:productId" element={<Product />} />
           </Routes>
         </>
       </div>
@@ -32,12 +36,6 @@ const App = () => {
       </div>
     </div>
   );
-}
+};
 
 export default App;
-
-
-
-
-
-
